486969718007daf4c358f13058666c27ab519245
Lawrence Kang <lkang@lcs.on.ca>
2023-10-26 14:16:30 -0400

    Initial Commit

---
+++ RotatingArrow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RotatingArrow
+//
+//  Created by Chen-Ning Kang on 2023-10-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RotatingArrow/RotatingArrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotatingArrowApp.swift
+//  RotatingArrow
+//
+//  Created by Chen-Ning Kang on 2023-10-26.
+//
+
+import SwiftUI
+
+@main
+struct RotatingArrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
