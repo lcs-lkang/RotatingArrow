908aa33b2a9611ab801ff77ee53822009144e98f
Lawrence Kang <lkang@lcs.on.ca>
2023-10-26 14:27:23 -0400

    rotating arrow

--- RotatingArrow/ContentView.swift
+++ RotatingArrow/ContentView.swift
@@ -1,21 +1,52 @@
 //
 //  ContentView.swift
-//  RotatingArrow
+//  ArrowExample
 //
-//  Created by Chen-Ning Kang on 2023-10-26.
+//  Created by Russell Gordon on 2023-10-26.
 //
-
 import SwiftUI
 
+struct Arrow: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        // Create a path
+        var path = Path()
+        
+        // Define the path
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY/3*2))
+        path.addLine(to: CGPoint(x: rect.maxX/3*2, y: rect.maxY/3*2))
+        path.addLine(to: CGPoint(x: rect.maxX/3*2, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY/2*1))
+        path.addLine(to: CGPoint(x: rect.maxX/3*2, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX/3*2, y: rect.maxY/3*1))
+        path.addLine(to: CGPoint(x: rect.maxX/3*2, y: rect.maxY/3*1))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY/3*1))
+//         Return a path
+        return path
+    }
+    
+}
+
 struct ContentView: View {
+    
+    @State var selectedAngle: Double = 0.0
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            Arrow()
+                .aspectRatio(1.0, contentMode: .fit)
+                .rotationEffect(.degrees(selectedAngle))
+            
+            Spacer()
+            
+            Slider(value: $selectedAngle, in: 0...360)
         }
         .padding()
+        
     }
 }
 
